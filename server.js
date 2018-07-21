f1652c46ca245f6be16038c533cbb43c54717768
unknown <t.bakeer4@gmail.com>
2018-07-20 23:54:27 -0700

    inizilaizing

---
+++ server.js
@@ -0,0 +1,24 @@
+
+var express=require("express");
+var bodyParser = require("body-parser");
+var parse=require("path");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
